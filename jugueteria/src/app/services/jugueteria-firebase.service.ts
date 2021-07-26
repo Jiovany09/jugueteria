d53a4e27592b6291009c1bf7151034fc0767211f
Jiovany Maruri <20183l301005@utcv.edu.mx>
2021-07-26 16:48:07 -0500

    Modificación del servicio jugueteria-firebase

--- jugueteria/src/app/services/jugueteria-firebase.service.ts
+++ jugueteria/src/app/services/jugueteria-firebase.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JugueteriaFirebaseService {
 
-  constructor() { }
+  constructor(private firestore: AngularFirestore) { }
+
+  public getJuguetes() {
+    return this.firestore.collection('juguetes').snapshotChanges();
+  }
 }
